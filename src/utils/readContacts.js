39d65459886a5f4d5c92c432a3918e4c4a73dc64
Nataliia Kruhliak <nataliia.kaer@gmail.com>
2024-09-26 23:01:58 +0300

    add first function for reading

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,10 @@
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const data = await PATH_DB.readFile('../db/db.json', 'utf8');
+    console.log('Вміст файлу: ', data);
+  } catch (error) {
+    console.log('Помилка читання файлу: ', error);
+  }
+};
